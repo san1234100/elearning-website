263c16835ffbd7133a49173745c22aff41743aa1
CHANDRU U <sanjeevchandru1234@gmail.com>
2024-02-22 15:14:30 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,6 @@
 import { defineConfig } from "vite";
-import { resolve } from "path";
+
 
 export default defineConfig({
   base: '/elearning-website/',
-    build: {
-      rollupOptions: {
-        input: {
-          main: resolve(__dirname, "index.html"),
-        },
-      },
-    },
 });
